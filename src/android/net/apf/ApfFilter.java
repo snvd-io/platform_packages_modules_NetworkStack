ec05c93dbf647eb3d0b5d55bedc80c6dddb8f6cb
Yuyang Huang <yuyanghuang@google.com>
2024-07-04 16:00:53 +0900

    Register IpClient as OffloadEngine when APF supports mDNS offload
    Updated IpClient to register itself as an OffloadEngine instance when
    the APF indicates support for mDNS offload functionality.
    
    The offloadEngine is registered when IpClient starts running and
    unregistered when it stops. However, the offloadEngine is also
    registered if the IpClient's ApfCapabilities are updated. This can
    happen when a WiFi STA starts in secondary mode and then switches
    to primary mode. If WiFi STA starts in secondary mode, then
    ApfCapabilities is null so there is no ApfFilter created when IpClient
    entered the RunningState.
    
    Test: TH
    Change-Id: I4a0c73a1ebdcd306f981236c0298b48414a563c4

--- src/android/net/apf/ApfFilter.java
+++ src/android/net/apf/ApfFilter.java
@@ -215,6 +215,7 @@ public class ApfFilter implements AndroidPacketFilter {
         public boolean hasClatInterface;
         public boolean shouldHandleArpOffload;
         public boolean shouldHandleNdOffload;
+        public boolean shouldHandleMdnsOffload;
     }
 
     // Thread to listen for RAs.
@@ -305,6 +306,7 @@ public class ApfFilter implements AndroidPacketFilter {
     private final int mAcceptRaMinLft;
     private final boolean mShouldHandleArpOffload;
     private final boolean mShouldHandleNdOffload;
+    private final boolean mShouldHandleMdnsOffload;
 
     private final NetworkQuirkMetrics mNetworkQuirkMetrics;
     private final IpClientRaInfoMetrics mIpClientRaInfoMetrics;
@@ -406,6 +408,7 @@ public class ApfFilter implements AndroidPacketFilter {
         mAcceptRaMinLft = config.acceptRaMinLft;
         mShouldHandleArpOffload = config.shouldHandleArpOffload;
         mShouldHandleNdOffload = config.shouldHandleNdOffload;
+        mShouldHandleMdnsOffload = config.shouldHandleMdnsOffload;
         mDependencies = dependencies;
         mNetworkQuirkMetrics = networkQuirkMetrics;
         mIpClientRaInfoMetrics = dependencies.getIpClientRaInfoMetrics();
@@ -2681,6 +2684,11 @@ public class ApfFilter implements AndroidPacketFilter {
     @Override
     public boolean supportNdOffload() {
         return shouldUseApfV6Generator() && mShouldHandleNdOffload;
+    }
+
+    @Override
+    public boolean shouldUseMdnsOffload() {
+        return shouldUseApfV6Generator() && mShouldHandleMdnsOffload;
     }
 
     private boolean shouldUseApfV6Generator() {
